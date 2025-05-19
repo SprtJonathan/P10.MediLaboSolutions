d1b8990f17c7b03844c996aebfdf5e63669a3f13
Jonathan Binot <73881990+SprtJonathan@users.noreply.github.com>
2025-05-19 09:52:58 +0200

    [S2-E3-U1] Mise en place de l'unicité des adresses

---
+++ MediLaboSolutions.API/Migrations/20250519074303_SetAdresseAsUnique.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediLaboSolutions.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetAdresseAsUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_AdresseId",
+                table: "Patients");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_AdresseId",
+                table: "Patients",
+                column: "AdresseId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_AdresseId",
+                table: "Patients");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_AdresseId",
+                table: "Patients",
+                column: "AdresseId",
+                unique: true,
+                filter: "[AdresseId] IS NOT NULL");
+        }
+    }
+}
